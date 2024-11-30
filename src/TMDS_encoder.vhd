98fee4b34c9a3435fc7304483ebb6e065748bbd4
Ludvig <ludvig.snihs@outlook.com>
2024-11-30 12:00:16 +0100

    Modified code

--- src/TMDS_encoder.vhd
+++ src/TMDS_encoder.vhd
@@ -21,13 +21,15 @@ architecture rtl of TMDS_encoder is
     signal w_XOR              : std_logic                    := '0';
     signal w_qm               : std_logic_vector(8 downto 0) := (others => '0');
     signal w_invert_qm        : std_logic                    := '0';
-    signal w_balance          : signed(3 downto 0)           := (others => '0');
+    signal w_balance          : signed(4 downto 0)           := (others => '0');
     signal w_balance_sign_eq  : std_logic                    := '0';
-    signal w_balance_acc      : unsigned(3 downto 0)         := (others => '0');
-    signal w_balance_acc_incr : unsigned(3 downto 0)         := (others => '0');
-    signal w_balance_acc_new  : unsigned(3 downto 0)         := (others => '0');
+    signal w_balance_acc      : signed(4 downto 0)           := (others => '0');
+    signal w_balance_acc_incr : signed(4 downto 0)           := (others => '0');
+    signal w_balance_acc_new  : signed(4 downto 0)           := (others => '0');
     signal r_cntr             : integer range -8 to 8        := 0;
-    signal TMDS_data          : std_logic_vector(9 downto 0) := (others => '0');
+    signal w_TMDS_data        : std_logic_vector(9 downto 0) := (others => '0');
+    signal w_balance_zero     : std_logic                    := '0';
+    signal w_C1_C0            : std_logic_vector(1 downto 0) := (others => '0');
 
 begin
     --------------------------------------------------------------------
@@ -59,7 +61,6 @@ begin
 
     -- 3. Need to check DC bias
     p_DC_bias : process (all)
-        variable v_balance_zero : std_logic := '0';
     begin
         -----------------------
         w_balance <= (others => '0') - to_signed(4, w_balance'length); -- 4 zeros, 4 ones
@@ -77,60 +78,47 @@ begin
         end if;
         -----------------------
         if (w_balance = 0) or (w_balance_acc = 0) then
-            v_balance_zero := '1';
+            w_balance_zero <= '1';
         else
-            v_balance_zero := '0';
+            w_balance_zero <= '0';
         end if;
         -----------------------
         if (w_invert_qm = '1') then
-            w_balance_acc_new <= w_balance_acc - w_balance_acc_incr;
+            w_balance_acc_new <= signed('0' & w_balance_acc) - w_balance_acc_incr;
         else
             w_balance_acc_new <= w_balance_acc + w_balance_acc_incr;
         end if;
         -----------------------
     end process p_DC_bias;
-    w_balance_sign_eq  <= w_balance(w_balance'high) and w_balance_acc(w_balance_acc'high); -- TODO might need extra bit for sign
-    w_balance_acc_incr <= w_balance - signed((w_qm(8) or not w_balance_sign_eq) and not v_balance_zero);
+    w_balance_sign_eq <= w_balance(w_balance'high) and w_balance_acc(w_balance_acc'high); -- might need extra bit for sign
 
-    TMDS_data <= w_invert_qm & w_qm(8) & (w_qm(7 downto 0) and w_invert_qm);
+    w_balance_acc_incr <= w_balance - ((w_qm(8) xor not w_balance_sign_eq) and not w_balance_zero); -- TODO
 
-        -- 4. Output (clocked)
-        process (clk)
-        begin
-            if rising_edge(clk) then
-                if (i_video_en = '1') then
-                    o_TMDS <= TMDS_data;
-                else
-                    case C1_C0 is
+    w_TMDS_data <= w_invert_qm & w_qm(8) & (w_qm(7 downto 0) xor (others => w_invert_qm)); -- TODO
+
+    -- 4. Output (clocked)
+    process (clk)
+    begin
+        if rising_edge(clk) then
+            if (i_video_en = '1') then
+                o_TMDS        <= w_TMDS_data;
+                w_balance_acc <= w_balance_acc_new;
+            else
+                w_balance_acc <= (others => '0');
+                case w_C1_C0 is
                     when "00" =>
-
+                        o_TMDS <= "1101010100";
                     when "01" =>
-
+                        o_TMDS <= "0010101011";
                     when "10" =>
-
+                        o_TMDS <= "0101010100";
                     when "11" =>
-
+                        o_TMDS <= "1010101011";
                     when others =>
-                            null;
-    end case;
+                        o_TMDS <= "1101010100"; -- 00 case
+                end case;
             end if;
-            end if;
-        end process;
-    else
-    case C1_C0 is
-            when  =>
-
-            when  =>
-
-            when  =>
-
-            when  =>
-
-            when others =>
-            null;
-            end case;
-    end if;
-end if;
-end process;
+        end if;
+    end process;
 
 end architecture;
