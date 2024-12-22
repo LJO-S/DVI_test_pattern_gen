29f85b99675c6faf68abe3ae262c9a29fff2fbc3
Ludvig Snihs <ludvig.snihs@outlook.com>
2024-12-22 20:00:13 +0100

    Change 8-bit LFSR to 32-bit to increase repetition period

--- src/galois_lfsr.vhd
+++ src/galois_lfsr.vhd
@@ -14,26 +14,41 @@ entity galois_lfsr is
 end entity galois_lfsr;
 
 architecture rtl of galois_lfsr is
-    signal r_lfsr : std_logic_vector(8 downto 1) := x"80";
+    --signal r_lfsr : std_logic_vector(8 downto 1) := x"80";
+
+    signal w_poly    : std_logic_vector(32 downto 1) := x"80000057";
+    signal w_mask    : std_logic_vector(32 downto 1) := (others => '0');
+    signal r_lfsr    : std_logic_vector(32 downto 1) := x"80000000";
+    signal r_counter : integer range 0 to 250_000    := 0;
 
 begin
-    process (i_pixclk)
+
+    g_mask : for i in 32 downto 1 generate
+        w_mask(i) <= r_lfsr(1) and w_poly(i);
+    end generate;
+
+    p_lfsr : process (i_pixclk)
     begin
         if rising_edge(i_pixclk) then
             if (i_en = '1') then
-                r_lfsr(8) <= r_lfsr(1); -- Feedback
-                r_lfsr(7) <= r_lfsr(8);
-                r_lfsr(6) <= r_lfsr(7) xor r_lfsr(1);
-                r_lfsr(5) <= r_lfsr(6) xor r_lfsr(1);
-                r_lfsr(4) <= r_lfsr(5) xor r_lfsr(1);
-                r_lfsr(3) <= r_lfsr(4);
-                r_lfsr(2) <= r_lfsr(3);
-                r_lfsr(1) <= r_lfsr(2);
+                if (r_counter = 250_000) then
+                    -- r_lfsr(32) <= 0 xor r_lfsr(1) = r_lfsr(1);
+                    -- r_lfsr(31) <= r_lfsr(32);
+                    -- r_lfsr(30) <= r_lfsr(31);
+                    -- r_lfsr(29) <= r_lfsr(30);
+                    -- ...
+                    -- r_lfsr(2) <= r_lfsr(3);
+                    -- r_lfsr(1) <= r_lfsr(2);
+                    r_lfsr <= ('0' & r_lfsr(r_lfsr'left downto 2)) xor w_mask;
+                else
+                    r_counter <= r_counter + 1;
+                end if;
             else
-                r_lfsr <= x"80";
+                r_counter <= 0;
+                r_lfsr    <= x"DEADBEEF";
             end if;
         end if;
     end process;
-    o_lfsr <= r_lfsr(8 downto 1);
 
+    o_lfsr <= r_lfsr(8 downto 1);
 end architecture;
