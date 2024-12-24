0e73994b187513acfc8f50cbacbbd4eec88803fb
Ludvig <ludvig.snihs@outlook.com>
2024-12-24 10:53:57 +0100

    Add internal interface support for breakout game

--- src/project_top.vhd
+++ src/project_top.vhd
@@ -2,14 +2,15 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+use work.breakout_pkg.all;
+
 entity project_top is
     port (
         clk : in std_logic; -- 125 MHz clk
 
-        i_pattern_0 : in std_logic;
-        i_pattern_1 : in std_logic;
-        i_pattern_2 : in std_logic;
-        i_pattern_3 : in std_logic;
+        i_LEFT       : in std_logic;
+        i_RIGHT      : in std_logic;
+        i_GAME_START : in std_logic;
 
         --o_HDMI_HPD : out std_logic;
 
@@ -28,23 +29,39 @@ entity project_top is
 end entity project_top;
 
 architecture rtl of project_top is
+    constant c_VIDEO_WIDTH : integer := 8;
+    constant c_TOTAL_COLS  : integer := 800;
+    constant c_TOTAL_ROWS  : integer := 525;
+    constant c_ACTIVE_COLS : integer := 640;
+    constant c_ACTIVE_ROWS : integer := 480;
+
     signal w_pixclk   : std_logic;
     signal w_TMDS_clk : std_logic;
     signal w_HDMI_HPD : std_logic;
 
-    signal w_pattern_0_db : std_logic;
-    signal w_pattern_1_db : std_logic;
-    signal w_pattern_2_db : std_logic;
-    signal w_pattern_3_db : std_logic;
-    signal w_counter_X    : unsigned(9 downto 0);
-    signal w_counter_Y    : unsigned(9 downto 0);
-    signal w_HSYNC        : std_logic;
-    signal w_VSYNC        : std_logic;
-    signal w_draw         : std_logic;
-    signal w_video_red    : std_logic_vector(7 downto 0);
-    signal w_video_grn    : std_logic_vector(7 downto 0);
-    signal w_video_blu    : std_logic_vector(7 downto 0);
-    signal w_temp         : unsigned(6 downto 0);
+    signal w_LEFT                 : std_logic;
+    signal w_RIGHT                : std_logic;
+    signal w_GAME_START           : std_logic;
+    signal w_HSYNC_pre_porch      : std_logic;
+    signal w_VSYNC_pre_porch      : std_logic;
+    signal w_video_red_pre_porch  : std_logic_vector(7 downto 0);
+    signal w_video_grn_pre_porch  : std_logic_vector(7 downto 0);
+    signal w_video_blu_pre_porch  : std_logic_vector(7 downto 0);
+    signal w_HSYNC_breakout       : std_logic;
+    signal w_VSYNC_breakout       : std_logic;
+    signal w_HSYNC_post_porch     : std_logic;
+    signal w_VSYNC_post_porch     : std_logic;
+    signal w_video_red_post_porch : std_logic_vector(7 downto 0);
+    signal w_video_grn_post_porch : std_logic_vector(7 downto 0);
+    signal w_video_blu_post_porch : std_logic_vector(7 downto 0);
+
+    signal w_HSYNC     : std_logic;
+    signal w_VSYNC     : std_logic;
+    signal w_draw      : std_logic;
+    signal w_video_red : std_logic_vector(7 downto 0);
+    signal w_video_grn : std_logic_vector(7 downto 0);
+    signal w_video_blu : std_logic_vector(7 downto 0);
+    signal w_temp      : unsigned(6 downto 0);
 
     signal w_video_0_p : std_logic;
     signal w_video_0_n : std_logic;
@@ -67,7 +84,7 @@ begin
         port map
         (
             i_CLK      => clk,
-            o_pixclk => w_pixclk,
+            o_pixclk   => w_pixclk,
             o_TMDS_clk => w_TMDS_clk
         );
     --------------------------------------------------------------------
@@ -77,12 +94,12 @@ begin
         (
             i_TMDS_clk  => w_TMDS_clk,
             i_pixclk    => w_pixclk,
-            i_HSYNC     => w_HSYNC,
-            i_VSYNC     => w_VSYNC,
+            i_HSYNC     => not w_HSYNC_post_porch,
+            i_VSYNC     => not w_VSYNC_post_porch,
             i_draw      => w_draw,
-            i_video_red => w_video_red,
-            i_video_grn => w_video_grn,
-            i_video_blu => w_video_blu,
+            i_video_red => w_video_red_post_porch,
+            i_video_grn => w_video_grn_post_porch,
+            i_video_blu => w_video_blu_post_porch,
             temp        => open,
             o_TMDS      => w_TMDS,
             o_TMDS_clk  => w_TMDS_out_clk,
@@ -123,6 +140,7 @@ begin
         );
     --------------------------------------------------------------------
     --------------------------------------------------------------------
+    /*
     pattern_generator_inst : entity work.pattern_generator
         port map
         (
@@ -141,35 +159,101 @@ begin
             o_video_blu    => w_video_blu,
             temp           => open
         );
+    */
+
+    -- TODO 
+    -- 1. Assign generics ports
+    -- 2. Port map output from sync_porch into TMDS_top
+    -- 3. Fix color output to match our 8-bit instead of 4-bit (g_VIDEO_WIDTH)
+    -- 4. Remember to invert HSYNC/VSYNC into TMDS
+    -- 5. Fix i_draw to enable when we want in SYNC_PORCH
+    breakout_top_inst : entity work.breakout
+        generic map(
+            g_VIDEO_WIDTH     => c_VIDEO_WIDTH,
+            g_TOTAL_COLS      => c_TOTAL_COLS,
+            g_TOTAL_ROWS      => c_TOTAL_ROWS,
+            g_ACTIVE_COLS     => c_ACTIVE_COLS,
+            g_ACTIVE_ROWS     => c_ACTIVE_ROWS,
+            g_PLAYER_paddle_Y => c_PLAYER_paddle_Y
+        )
+        port map
+        (
+            i_CLK        => w_pixclk,
+            i_HSYNC      => w_HSYNC_breakout, -- from SYNC_PULSE
+            i_VSYNC      => w_VSYNC_breakout,
+            i_RIGHT      => w_RIGHT,
+            i_LEFT       => w_LEFT,
+            i_game_start => w_game_start,
+            o_HSYNC      => w_HSYNC_pre_porch, -- into porch
+            o_VSYNC      => w_VSYNC_pre_porch,
+            o_RED_VIDEO  => w_video_red_pre_porch, -- into porch
+            o_GRN_VIDEO  => w_video_grn_pre_porch,
+            o_BLU_VIDEO  => w_video_blu_pre_porch
+        );
+
+    -- INTO BREAKOUT
+    VGA_sync_pulses_inst : entity work.VGA_sync_pulses
+        generic map(
+            g_TOTAL_COLS  => c_TOTAL_COLS,
+            g_TOTAL_ROWS  => c_TOTAL_ROWS,
+            g_ACTIVE_ROWS => c_ACTIVE_ROWS,
+            g_ACTIVE_COLS => c_ACTIVE_COLS
+        )
+        port map
+        (
+            i_CLK       => w_pixclk,
+            o_row_count => open,
+            o_col_count => open,
+            o_HSYNC     => w_HSYNC_breakout,
+            o_VSYNC     => w_VSYNC_breakout
+        );
+
+    -- INTO TMDS_encoder
+    VGA_sync_porch_inst : entity work.VGA_sync_porch
+        generic map(
+            g_VIDEO_WIDTH => c_VIDEO_WIDTH,
+            g_TOTAL_COLS  => c_TOTAL_COLS,
+            g_TOTAL_ROWS  => c_TOTAL_ROWS,
+            g_ACTIVE_COLS => c_ACTIVE_COLS,
+            g_ACTIVE_ROWS => c_ACTIVE_ROWS
+        )
+        port map
+        (
+            i_CLK       => w_pixclk,
+            i_HSYNC     => w_HSYNC_pre_porch,
+            i_VSYNC     => w_VSYNC_pre_porch,
+            o_HSYNC     => w_HSYNC_post_porch,
+            o_VSYNC     => w_VSYNC_post_porch,
+            i_RED_video => w_video_red_pre_porch,
+            i_GRN_video => w_video_grn_pre_porch,
+            i_BLU_video => w_video_blu_pre_porch,
+            o_RED_video => w_video_red_post_porch,
+            o_GRN_video => w_video_grn_post_porch,
+            o_BLU_video => w_video_blu_post_porch,
+            o_draw      => w_draw
+        );
     --------------------------------------------------------------------
     --------------------------------------------------------------------
     PB_debounce_inst_0 : entity work.PB_debounce
         port map
         (
             i_CLK         => w_pixclk,
-            i_PB          => i_pattern_0,
-            o_PB_debounce => w_pattern_0_db
+            i_PB          => i_LEFT,
+            o_PB_debounce => w_LEFT
         );
     PB_debounce_inst_1 : entity work.PB_debounce
         port map
         (
             i_CLK         => w_pixclk,
-            i_PB          => i_pattern_1,
-            o_PB_debounce => w_pattern_1_db
+            i_PB          => i_RIGHT,
+            o_PB_debounce => w_RIGHT
         );
     PB_debounce_inst_2 : entity work.PB_debounce
         port map
         (
             i_CLK         => w_pixclk,
-            i_PB          => i_pattern_2,
-            o_PB_debounce => w_pattern_2_db
-        );
-    PB_debounce_inst_3 : entity work.PB_debounce
-        port map
-        (
-            i_CLK         => w_pixclk,
-            i_PB          => i_pattern_3,
-            o_PB_debounce => w_pattern_3_db
+            i_PB          => i_GAME_START,
+            o_PB_debounce => w_GAME_START
         );
     --------------------------------------------------------------------
     --------------------------------------------------------------------
